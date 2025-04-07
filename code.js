94577019463c65a244e314015f16b07f6f0d4fb9
aijun-hall-uwyo <aijun.hall@uwyo.edu>
2025-04-07 15:00:41 -0600

    working code and tests

--- code.js
+++ code.js
@@ -1,3 +1,54 @@
 function quicksort(array) {
+    // Base Case: 1 or 0 elements in array, already "sorted"
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const stack = [];
+    stack.push(0);
+    stack.push(array.length - 1);
+
+    while (stack.length > 0) {
+        let endIndex = stack.pop();
+        let startIndex = stack.pop();
+
+        // Base Case: 1 or 0 elements in array, already "sorted"
+        if (startIndex >= endIndex) {
+            continue;
+        }
+
+        // Choose pivot as last element in current subarray
+        let pivot = array[endIndex];
+
+        // Parition array with pivot included on left side
+        let i = startIndex - 1;
+        for (let j = startIndex; j < endIndex; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                // Swap values in array
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Recombine around pivot
+        let temp = array[i + 1];
+
+        array[i + 1] = array[endIndex];
+        array[endIndex] = temp;
+
+        let pivotIndex = i + 1;
+
+        // Push right side
+        stack.push(pivotIndex + 1);
+        stack.push(endIndex);
+
+        // Push left side
+        stack.push(startIndex);
+        stack.push(pivotIndex - 1);
+    }
+
     return array;
 }
--- code.test.js
+++ code.test.js
@@ -3,6 +3,7 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
+// Test 1: Output of quicksort should match javascript built in sort
 const testSort =
     jsc.forall("array nat", function(arr) {
         var a1 = JSON.parse(JSON.stringify(arr));
@@ -12,4 +13,39 @@ const testSort =
                 { return a - b; }));
     });
 
+// Test 2: Check that elements in the output of quicksort are in ASCENDING order
+const testAscendingOrder =
+    jsc.forall("array nat", function(arr) {
+        var sortedArray = quicksort(arr.slice());
+        for (let i = 1; i < sortedArray.length; i++) {
+            if (sortedArray[i - 1] > sortedArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+// Test 3: Hardcoded value test
+const testHardcoded =
+    jsc.forall(jsc.constant([5, 2, 9, 1, 5, 6]), function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = [1, 2, 5, 5, 6, 9];
+        return JSON.stringify(quicksort(a1)) === JSON.stringify(a2);
+    });
+
+// Test 4: Hardcoded value test with duplicates
+const testHardcodedDuplicates =
+    jsc.forall(jsc.constant([1, 1, 2, 2, 3, 3]), function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = [1, 1, 2, 2, 3, 3];
+        return JSON.stringify(quicksort(a1)) === JSON.stringify(a2);
+    });
+
 jsc.assert(testSort);
+console.log('passed testSort')
+jsc.assert(testAscendingOrder);
+console.log('passed testAscendingOrder')
+jsc.assert(testHardcoded);
+console.log('passed testHardcoded')
+jsc.assert(testHardcodedDuplicates)
+console.log('passed testHardcodedDuplicates')
